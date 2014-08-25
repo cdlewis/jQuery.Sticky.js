3dc167814b1826f3f0f27072fca0987f24dacbbb
Chris Lewis <chris@Chris-Laptop.local>
2014-08-25 12:36:53 -0400

    final

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,9 +1,9 @@
 $.fn.sticky = function( offset ) {
   this.css( "position", "relative" );
-  var initial_position = this.offset().top
-    , element = this
+  var element = this
     , offset = offset || 0;
   $( window ).scroll( function( event ) {
+      var initial_position = element.offset().top - ( parseInt( element.css( "top" ), 10 ) || 0);
       if( window.scrollY + offset > initial_position ) {
             element.css( "top", ( window.scrollY - initial_position + offset ) + "px" );
           } else {
