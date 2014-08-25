c84c14dba16979c3d8dbe4e063523793383d8579
Chris Lewis <chris@Chris-Laptop.local>
2014-08-25 14:01:13 -0400

    limits!

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,13 +1,17 @@
-$.fn.sticky = function( offset ) {
+$.fn.sticky = function( offset, limit ) {
   this.css( "position", "relative" );
   var element = this
     , offset = offset || 0;
   $( window ).scroll( function( event ) {
-      var initial_position = element.offset().top - ( parseInt( element.css( "top" ), 10 ) || 0);
-      if( window.scrollY + offset > initial_position ) {
-            element.css( "top", ( window.scrollY - initial_position + offset ) + "px" );
-          } else {
-                element.css( "top", "initial" );
-              }
-    } );
+    var initial_position = element.offset().top - ( parseInt( element.css( "top" ), 10 ) || 0);
+    if( ( window.scrollY + offset ) > initial_position ) {
+      if( typeof( limit ) !== 'undefined' && ( ( window.scrollY + offset + element.height() ) > limit.offset().top ) ) {
+        return; // do nothing
+      } else {
+        element.css( "top", ( window.scrollY - initial_position + offset ) + "px" );
+      }
+    } else {
+      element.css( "top", "initial" );
+    }
+  } );
 };
