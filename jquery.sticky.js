804d3e5da12797ad1827d21288228ba371f2cc8f
Chris Lewis <chris@Chris-Laptop.local>
2014-08-25 12:08:03 -0400

    fixes!

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,11 +1,13 @@
 $.fn.sticky = function( offset ) {
   this.css( "position", "relative" );
-  var initial_position = this.offset().top + ( offset || 0 )
-    , element = this;
+  var initial_position = this.offset().top
+    , element = this
+    , offset = offset || 0;
   $( window ).scroll( function( event ) {
-    if( window.scrollY > initial_position ) {
-      element.css( "top", ( window.scrollY - initial_position ) + "px" );
-    }
-  } );
+      if( window.scrollY > initial_position ) {
+            element.css( "top", ( window.scrollY - initial_position + offset ) + "px" );
+          } else {
+                element.css( "top", "initial" );
+              }
+    } );
 };
-
