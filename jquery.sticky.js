4ea024b7807f76ad6b6822972b3879f465975b4a
Chris Lewis <chris@Chris-Laptop.local>
2014-10-09 14:17:49 -0400

    probably shouldnt assume that $ exists and is set to jQuery

--- jquery.sticky.js
+++ jquery.sticky.js
@@ -1,17 +1,19 @@
-$.fn.sticky = function( offset, limit ) {
-  this.css( "position", "relative" );
-  var element = this
-    , offset = offset || 0;
-  $( window ).scroll( function( event ) {
-    var initial_position = element.offset().top - ( parseInt( element.css( "top" ), 10 ) || 0);
-    if( ( window.pageYOffset + offset ) > initial_position ) {
-      if( typeof( limit ) !== 'undefined' && ( ( window.pageYOffset + offset + element.height() ) > limit.offset().top ) ) {
-        return; // do nothing
+( function( $ ) {
+  $.fn.sticky = function( offset, limit ) {
+    this.css( "position", "relative" );
+    var element = this
+      , offset = offset || 0;
+    $( window ).scroll( function( event ) {
+      var initial_position = element.offset().top - ( parseInt( element.css( "top" ), 10 ) || 0);
+      if( ( window.pageYOffset + offset ) > initial_position ) {
+        if( typeof( limit ) !== 'undefined' && ( ( window.pageYOffset + offset + element.height() ) > limit.offset().top ) ) {
+          return; // do nothing
+        } else {
+          element.css( "top", ( window.pageYOffset - initial_position + offset ) + "px" );
+        }
       } else {
-        element.css( "top", ( window.pageYOffset - initial_position + offset ) + "px" );
+        element.css( "top", "initial" );
       }
-    } else {
-      element.css( "top", "initial" );
-    }
-  } );
-};
+    } );
+  };
+} )( jQuery );
